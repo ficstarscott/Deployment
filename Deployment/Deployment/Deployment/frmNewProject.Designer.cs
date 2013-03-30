abce504cc07a30ec06f6eeba494b57d8fa4d60a6
Scott Vahey <scott.vahey@rogers.com>
2013-03-30 12:17:09 -0400

    Add new project

--- Deployment/Deployment/Deployment/frmNewProject.Designer.cs
+++ Deployment/Deployment/Deployment/frmNewProject.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Deployment
 {
-    partial class Form1
+    partial class frmNewProject
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frmNewProject
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(415, 312);
+            this.Name = "frmNewProject";
+            this.Text = "New Project";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
     }
 }
-
--- Deployment/Deployment/Deployment/frmNewProject.cs
+++ Deployment/Deployment/Deployment/frmNewProject.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Deployment
 {
-    public partial class Form1 : Form
+    public partial class frmNewProject : Form
     {
-        public Form1()
+        public frmNewProject()
         {
             InitializeComponent();
         }
